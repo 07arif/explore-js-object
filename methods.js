1a7f436de7ff28c2eecda55b7833794a825c1bd4
07arif <ai.arif0711@gmail.com>
2022-09-02 22:58:22 +0600

    Object method access and set property values

--- methods.js
+++ methods.js
@@ -9,10 +9,18 @@ const student = {
     Improve: function (subject) {
         this.exam()
         return `${this.name} is takeing improvement exam on ${subject} `
+    },
+    treatDay: function (amount, tips) {
+        this.money = this.money - amount - tips;
+        return this.money;
     }
+
 }
 const exam = student.exam();
 console.log(exam)
 
 const reExam = student.Improve('Algebra')
 console.log(reExam);
+
+const remaining = student.treatDay(900, 100);
+console.log(remaining);
