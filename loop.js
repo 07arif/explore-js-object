5ba7a211370cbfe93200a66d839882d482445783
07arif <ai.arif0711@gmail.com>
2022-09-02 23:52:07 +0600

    Loop through an object using for in, for of, object entries

---
+++ loop.js
@@ -0,0 +1,19 @@
+const numbers = [12, 25, 65, 73, 54];
+/*
+for (const number of numbers) {
+    console.log(number);
+}
+*/
+
+const bottle = { color: 'yello', price: 50, isCleaned: true, capacity: 1 };
+const keys = Object.keys(bottle);
+console.log(keys);
+/*
+3 way to read object properties
+1.bottle.color
+2. bottle['color']
+3.bottle[key]
+*/
+for (const key of keys) {
+    console.log(key, bottle[key]);
+}
