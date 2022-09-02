0c1fb465869aa4ff28ae5ef282bd6beac78a60e0
07arif <ai.arif0711@gmail.com>
2022-09-02 23:24:12 +0600

    freez, delete,

--- freeze.js
+++ freeze.js
@@ -5,4 +5,11 @@ const values = Object.values(bottle);
 // console.log(values);
 
 const pair = Object.entries(bottle);
-console.log(pair)
+// console.log(pair)
+
+console.log(bottle);
+// delete bottle.isCleaned;
+Object.freeze(bottle);
+bottle.price = 1500;
+bottle.height = 12;
+console.log(bottle);
