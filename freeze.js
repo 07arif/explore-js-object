d734fa0b9a1331d1341719010ef585636a16dbaa
07arif <ai.arif0711@gmail.com>
2022-09-02 23:13:59 +0600

    entries

---
+++ freeze.js
@@ -0,0 +1,8 @@
+const bottle = { color: 'yello', price: 50, isCleaned: true, capacity: 1 };
+const keys = Object.keys(bottle);
+// console.log(keys);
+const values = Object.values(bottle);
+// console.log(values);
+
+const pair = Object.entries(bottle);
+console.log(pair)
